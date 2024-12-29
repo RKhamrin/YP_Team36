a5cd2ab854a95680e47c72cd479951a50eff4409
RKhamrin <151268414+RKhamrin@users.noreply.github.com>
2024-12-30 02:44:40 +0300

    Исправить api_route.py в соответствии с проверкой кода

--- model_trainer/api/api_route.py
+++ model_trainer/api/api_route.py
@@ -1,23 +1,20 @@
 from typing import Union, Dict, List
-
-from fastapi import APIRouter, HTTPException, File, UploadFile
-from fastapi.responses import StreamingResponse
-import io
-from http import HTTPStatus
-
-from pydantic import BaseModel
-import pickle 
-
+import pickle
 import json
 
+import io
+from http import HTTPStatus
+from fastapi import APIRouter, HTTPException, UploadFile
+from fastapi.responses import StreamingResponse
+
+from pydantic import BaseModel
+
 import pandas as pd
-import numpy as np
 
 from sklearn.preprocessing import StandardScaler, OneHotEncoder
 from sklearn.linear_model import LogisticRegression
 
 from api.utils import GetTrain, GetPrediction
-from dataclasses import dataclass
 
 
 router = APIRouter(prefix='/api/models')
@@ -25,13 +22,14 @@ router = APIRouter(prefix='/api/models')
 models = {}
 models_load = {}
 
-with open("data/baseline_model.pkl", 'rb') as model_file:
-    loaded_model = pickle.load(model_file)
-with open("data/baseline_ohe.pkl", 'rb') as ohe_file:
-    loaded_ohe = pickle.load(ohe_file)
-with open("data/baseline_scaler.pkl", 'rb') as model_file:
-    loaded_scaler = pickle.load(model_file)
-models['baseline_model'] = [loaded_model, loaded_ohe, loaded_scaler, {}]
+with open("data/baseline_model.pkl", 'rb') as baseline_model:
+    baseline_model = pickle.load(baseline_model)
+with open("data/baseline_ohe.pkl", 'rb') as baseline_ohe:
+    baseline_ohe = pickle.load(baseline_ohe)
+with open("data/baseline_scaler.pkl", 'rb') as baseline_scaler:
+    baseline_scaler = pickle.load(baseline_scaler)
+models['baseline_model'] = [baseline_model, baseline_ohe, baseline_scaler, {}]
+
 
 class Item(BaseModel):
     Attendance: int
@@ -70,40 +68,48 @@ class Item(BaseModel):
     venue: str
     opponent: str
 
+
 class Items(BaseModel):
     objects: List[Item]
+
 
 class ModelConfig(BaseModel):
     id: str
     ml_model_type: str
     hyperparameters: Dict
 
+
 class FitResponse(BaseModel):
     message: str
+
 
 class ShowModelsResponse(BaseModel):
     message: Dict
 
+
 class SetModelRequest(BaseModel):
     id: str
 
+
 class SetModelResponse(BaseModel):
     message: str
+
 
 class ValidationError(HTTPException):
     loc: List[Union[str, int]]
     msg: str
     type: str
 
+
 class ApiResponse(BaseModel):
     message: str
     data: Union[Dict, None] = None
+
 
 # API endpoints
 @router.get("/show_example")
 async def show_example() -> StreamingResponse:
     """Функция получения примера (сэмпла) данных
-    
     returns:
         data: csv
     """
@@ -119,9 +125,10 @@ async def show_example() -> StreamingResponse:
     response.headers["Content-Disposition"] = "attachment; filename=sample_data.csv"
     return response
 
+
 @router.post("/fit", response_model=FitResponse, status_code=HTTPStatus.CREATED)
-async def fit(jsonfile: str, data: UploadFile):
-    """Функция обучения модели 
+async def fit(jsonfile: str, data: UploadFile) -> StreamingResponse:
+    """Функция обучения модели
     params:
         jsonfile: {'model_id': str, 'hyperparameters': Dict}
         data: csv
@@ -130,24 +137,21 @@ async def fit(jsonfile: str, data: UploadFile):
         {"message": "Model model_id is trained and saved"}
     """
     data = pd.read_csv(data.file, index_col=0).reset_index().drop(columns=['index'])
-    data.sort_values(by = 'date', ignore_index = 'True', inplace = True)
+    data.sort_values(by='date', ignore_index='True', inplace=True)
     config = json.loads(jsonfile)
     model_id, hyperparameters = config['model_id'], config['hyperparameters']
 
     feat_data = GetTrain(data)
-    print(len(data.columns))
-    x = pd.DataFrame(feat_data, columns = data.drop(['team','date', 'opponent', 'venue', 'result'], axis = 1).columns)
-    x = pd.concat([x, data['venue']], axis = 1)
+    x = pd.DataFrame(feat_data, columns=data.drop(['team', 'date', 'opponent', 'venue', 'result'], axis=1).columns)
+    x = pd.concat([x, data['venue']], axis=1)
 
-    for_enc = x[['venue']]
-    enc = OneHotEncoder(handle_unknown='ignore',drop = 'first', sparse_output = False)
-    encd = enc.fit_transform(for_enc)
+    enc = OneHotEncoder(handle_unknown='ignore', drop='first', sparse_output=False)
+    encd = enc.fit_transform(x[['venue']])
     one_hot_df = pd.DataFrame(encd, columns=enc.get_feature_names_out(['venue']))
 
-    x = pd.concat([x.drop(['venue'], axis = 1),one_hot_df], axis = 1)
-    print(data.columns)
+    x = pd.concat([x.drop(['venue'], axis=1), one_hot_df], axis=1)
     y = list(map((lambda i: 1 if i == 'W' else 0), data['result']))
-    
+
     normalizer = StandardScaler()
     scaler = normalizer.fit(x)
     x = scaler.transform(x)
@@ -155,7 +159,6 @@ async def fit(jsonfile: str, data: UploadFile):
     model = LogisticRegression(**hyperparameters)
     model.fit(x, y)
 
-    global models
     models[model_id] = hyperparameters
 
     with open(f"data/{model_id}_model.pkl", 'wb') as model_file:
@@ -166,10 +169,11 @@ async def fit(jsonfile: str, data: UploadFile):
         pickle.dump(scaler, scaler_file)
     return {'message': f'model {model_id} is trained and saved'}
 
+
 @router.post("/predict")
 async def predict(jsonfile: str, data: UploadFile) -> StreamingResponse:
     """Функция получения предсказаний загруженной модели
-    params: 
+    params:
         jsonfile: {'model_id': str}
         data: csv
 
@@ -177,10 +181,8 @@ async def predict(jsonfile: str, data: UploadFile) -> StreamingResponse:
         data: csv (со столбцом preds -- предсказания)
     """
     data = pd.read_csv(data.file, index_col=0).reset_index().drop(columns=['index'])
-    data.sort_values(by = 'date', ignore_index = 'True', inplace = True)
-    config = json.loads(jsonfile)
-    model_id = config['model_id']
-    print(loaded_model)
+    data.sort_values(by='date', ignore_index='True', inplace=True)
+    model_id = json.loads(jsonfile)['model_id']
     with open(f"data/{model_id}_model.pkl", 'rb') as model_file:
         model = pickle.load(model_file)
     with open(f"data/{model_id}_ohe.pkl", 'rb') as ohe_file:
@@ -197,6 +199,7 @@ async def predict(jsonfile: str, data: UploadFile) -> StreamingResponse:
     response.headers["Content-Disposition"] = "attachment; filename=result.csv"
     return response
 
+
 @router.get("/show_models", response_model=ShowModelsResponse)
 async def show_models():
     """Функция получения списка моделей
@@ -206,14 +209,15 @@ async def show_models():
     """
 
     models_info = {}
-    for model_id in models:
-        models_info[model_id] = {'hyperparameters': models[model_id]}
+    for model_id, hyps in models.items():
+        models_info[model_id] = {'hyperparameters': hyps}
 
-    return ShowModelsResponse(message = models_info)
+    return ShowModelsResponse(message=models_info)
+
 
 @router.post("/set_model", response_model=SetModelResponse)
 async def set_model(request: SetModelRequest):
-    """Функция загрузки модели 
+    """Функция загрузки модели
     params:
         model_id: str
 
@@ -223,7 +227,6 @@ async def set_model(request: SetModelRequest):
     """
 
     model_id = request.model_dump()['id']
-    global models_load
     models_load[model_id] = models[model_id]
 
-    return SetModelResponse(message = f'Model {model_id} is loaded')
+    return SetModelResponse(message=f'Model {model_id} is loaded')
